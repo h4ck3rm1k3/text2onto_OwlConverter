6eedded59017dbe64a859691c8938a7c5ecf6631
Thorsten Jerosch <thorsten.jerosch@elfin.de>
2014-10-31 12:55:32 +0100

    introduced some error checking

--- plotOWL.sh
+++ plotOWL.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
-echo "This script depends on raptor2 and graphviz";
-echo "Usage: ./plotOWL.sh <INFILE>";
-echo "The resulting image will be saved in the directory of <INFILE>";
+if [ -z "$1" ] ; then
+    echo "Error: No Input argument supplied!";
+    echo "Usage: ./plotOWL.sh <OWL_FILE>";
+    echo "Where OWL_FILE is an RDF formatted ontology file.";
+    exit -1;
+fi
+
+if [ ! -f $1 ] ; then
+    echo "Error: Invalid input file! Needs to be RDF formatted ontology file.";
+    exit -2;
+elif ! type -P rapper 2>/dev/null; then
+    echo "Please install raptor2!";
+    exit -3;
+elif ! type -P dot 2>/dev/null; then
+    echo "Please install GraphViz!";
+    exit -4;
+fi
 
 rapper -o dot $1 | dot -Tpng -o $1.png
+
+echo "Plot was saved as $1.png";
+
